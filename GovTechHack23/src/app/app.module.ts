fb835fc11a35ae79fe51eb7e8a30950b6ef3b660
liilii-ii <lili.bachmann@protonmail.com>
2023-03-23 19:54:57 +0100

    updated mission comp and added state dialog

--- GovTechHack23/src/app/app.module.ts
+++ GovTechHack23/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MissionComponent } from './mission/mission.component';
 import { MapComponent } from './map/map.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MapViewComponent } from './map-view/map-view.component';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { StateDialogComponent } from './state-dialog/state-dialog.component';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
     MissionComponent,
     MapComponent,
-    MapViewComponent
+    MapViewComponent,
+    StateDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,8 +33,11 @@ import { environment } from 'src/environments/environment';
     MatButtonModule,
     MatIconModule,
     MatTabsModule,
+    MatDialogModule,
+    FormsModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- GovTechHack23/src/app/mission/mission.component.ts
+++ GovTechHack23/src/app/mission/mission.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { StateDialogComponent } from '../state-dialog/state-dialog.component';
+import { States, StatesModel } from '../state-dialog/state-dialog.models';
 
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.component.html',
-  styleUrls: ['./mission.component.scss']
+  styleUrls: ['./mission.component.scss'],
 })
 export class MissionComponent implements OnInit {
+  subMissions: { tab: string }[] = [
+    { tab: 'Zone A' },
+    { tab: 'Zone B' },
+    { tab: 'Zone C' },
+    { tab: 'Zone D' },
+  ];
 
-  public subMissions: { tab: string }[] = [{tab: "Zone A"}, {tab: "Zone B"}, {tab: "Zone C"}, {tab: "Zone D"}];
+  state: StatesModel = { id: States.Started, text: 'Suche gestartet' };
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(StateDialogComponent, {
+      data: { state: this.state },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.state = result;
+    });
   }
-
 }
