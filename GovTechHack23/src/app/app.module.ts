a2aa7fb7d7642e9062c1153a578a0e99fba9e9e1
mirj <mirjamthomet@gmail.com>
2023-03-23 16:17:33 +0100

    feat: add map view component and material

--- GovTechHack23/src/app/app.module.ts
+++ GovTechHack23/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MissionComponent } from './mission/mission.component';
 import { MapComponent } from './map/map.component';
+import {MatIconModule} from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MapViewComponent } from './map-view/map-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MissionComponent,
-    MapComponent
+    MapComponent,
+    MapViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+MatButtonModule,
+MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
