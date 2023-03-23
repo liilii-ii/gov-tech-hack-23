d0adb53856bbf66ea29bd13de24975e07be505d1
mirj <mirjamthomet@gmail.com>
2023-03-23 15:38:45 +0100

    feat: add routing

--- GovTechHack23/src/app/app.module.ts
+++ GovTechHack23/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MissionComponent } from './mission/mission.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MissionComponent
+    MissionComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
