60fa2ee3b05b2b1f51b1bed6296fe42eedf171d7
mirj <mirjamthomet@gmail.com>
2023-03-23 19:43:25 +0100

    feat: link between tabs and zones in route param

--- GovTechHack23/src/app/mission/mission.component.ts
+++ GovTechHack23/src/app/mission/mission.component.ts
@@ -1,5 +1,7 @@
 import { FirebaseDbService } from 'src/db/firebase-db.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-mission',
@@ -8,12 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MissionComponent implements OnInit {
 
-  public subMissions: { tab: string }[] = [{tab: "Zone A"}, {tab: "Zone B"}, {tab: "Zone C"}, {tab: "Zone D"}];
+  /**
+   * Aktives Missions Tab
+   */
+  public activeMissionId$: Observable<number> | undefined;
 
-  constructor(private firebaseDbService: FirebaseDbService) { }
+   /**
+   * Returns active tab index
+   */
+  public get getIndexOfActiveTab$(): Observable<number> {
+      if(!this.activeMissionId$) return of(0)
+      return this.activeMissionId$.pipe(map(id =>{
+        const tab = this.subMissions.find(t => t.id === id);
+        return tab ? this.subMissions.indexOf(tab) : 0
+      }))
+ }
+
+  public subMissions: { tab: string, id: number }[] = [{tab: "Zone A", id: 1}, {tab: "Zone B", id: 2}, {tab: "Zone C", id: 3}, {tab: "Zone D", id: 4}];
+
+  constructor(private firebaseDbService: FirebaseDbService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.firebaseDbService.getAll().subscribe(l => console.log(l))
+    this.firebaseDbService.getAll().subscribe(l => console.log(l));
+    this.activeMissionId$ = this.route.paramMap.pipe(map(params => Number(params.get('id'))))
   }
+
+ 
 
 }
